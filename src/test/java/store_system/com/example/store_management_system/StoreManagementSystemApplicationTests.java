635667d41df383070a97b0836a5c81c4f8953dd4
Cheaheang <cheaheangchoeng@gmail.com>
2024-12-17 07:12:19 +0700

    first commit

---
+++ src/test/java/store_system/com/example/store_management_system/StoreManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package store_system.com.example.store_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
